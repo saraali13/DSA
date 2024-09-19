4114fd4c7285a9c6ac60ab4f7f3814c8330f9d3d
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2024-09-20 00:52:34 +0500

    Update Sorting.cpp

--- Lab 4/Sorting.cpp
+++ Lab 4/Sorting.cpp
@@ -12,7 +12,7 @@ void BubbleSort(int arr[], int n)
     bool swaped=false;
     for(int i=0;i<n;i++)
     {
-        for(int j=0;j<n-i;j++)
+        for(int j=0;j<n-1;j++)
         {
             if(arr[j]>arr[j+1])
             {
