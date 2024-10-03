feee541b6446c2759373156bd7b7f413df900812
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2024-10-03 20:33:28 +0500

    Update Q4.cpp

--- Lab 6/Q4.cpp
+++ Lab 6/Q4.cpp
@@ -9,17 +9,17 @@ using namespace std;
 class stack 
 {
 public:
-    int top = -1;
+    int top;
     int* arr;
     int size;
 
-    stack(int size) : size(size) 
-	{
+    stack(int size) : size(size), top(-1) 
+    {
         arr = new int[size];
     }
 
     ~stack() 
-	{
+    {
         delete[] arr;
     }
 };
@@ -34,21 +34,22 @@ bool isFull(stack* s)
     return s->top == s->size - 1;
 }
 
-
 void push(stack* st, int val) 
 {
     if (isFull(st)) 
-	{
+    {
         cout << "Stack is full" << endl;
     } 
-	else 
-	{
+    else 
+    {
         st->arr[++st->top] = val;
     }
 }
 
-int pop(stack* st) {
-    if (isEmpty(st)) {
+int pop(stack* st) 
+{
+    if (isEmpty(st)) 
+    {
         cout << "Stack Underflow" << endl;
         return -1; 
     }
@@ -57,63 +58,73 @@ int pop(stack* st) {
 
 class queue 
 {
-    stack* s1 = new stack(10);
-    stack* s2 = new stack(10);
+    stack* s1; 
+    stack* s2; 
 
 public:
+    queue(int size) 
+    {
+        s1 = new stack(size);
+        s2 = new stack(size);
+    }
+
     ~queue() 
-	{
-        delete s1; 
-        delete s2; 
+    {
+        delete s1;
+        delete s2;
     }
 
     void enqueue(int val) 
-	{
+    {
         push(s1, val);
-        cout << val<<", ";
+        cout << val << ", ";
     }
+    
 
     void dequeue() 
-	{
+    {
         if (isEmpty(s1) && isEmpty(s2)) 
-		{
+        {
             cout << "Queue is empty" << endl;
             return;
         }
 
-        if (isEmpty(s2))
-		{
+        if (isEmpty(s2)) 
+        {
             while (!isEmpty(s1)) 
-			{
+            {
                 push(s2, pop(s1));
             }
         }
 
-        
         if (!isEmpty(s2)) 
-		{
+        {
             int val = pop(s2);
-            cout <<  val<<", ";
+            cout << val << ", ";
         }
     }
 };
 
 int main()
 {
-    queue q;
+    queue q(10); 
 
-    cout << "Before Dequeue: \n";
+    cout << "Enqueuing elements:\n";
     q.enqueue(1);
     q.enqueue(2);
     q.enqueue(3);
     q.enqueue(4);
     q.enqueue(5);
     q.enqueue(6);
-    cout << "\nAfter Dequeue: \n";
-    q.dequeue();
-    q.dequeue();
-    q.dequeue();
+    q.enqueue(7);
+    q.enqueue(8);
+    q.enqueue(9);
+    q.enqueue(10);
 
+    cout << "\nDequeuing elements:\n";
+    q.dequeue();
+    q.dequeue();
+    q.dequeue();
 
     return 0;
 }
