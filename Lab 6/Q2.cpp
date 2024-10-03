a15ce40bbbbad0c2d39863c0fb8f43a2b494317e
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2024-10-03 20:23:01 +0500

    Update Q2.cpp

--- Lab 6/Q2.cpp
+++ Lab 6/Q2.cpp
@@ -1,27 +1,27 @@
 /*
-Name:S. Sara Ali
-ID:23K-0070
+Name: S. Sara Ali
+ID: 23K-0070
 */
-
 
 #include <iostream>
 #include <string>
 
 using namespace std;
 
+template <typename T>
 class Stack
 {
 private:
     int top;
     int max_size;
-    char *arr;
+    T *arr;
 
 public:
     Stack(int size)
     {
         top = -1;
         max_size = size;
-        arr = new char[max_size];
+        arr = new T[max_size];
     }
 
     ~Stack()
@@ -29,11 +29,11 @@ public:
         delete[] arr;
     }
 
-    void push(char c)
+    void push(T value)
     {
         if (top < max_size - 1)
         {
-            arr[++top] = c;
+            arr[++top] = value;
         }
         else
         {
@@ -41,7 +41,7 @@ public:
         }
     }
 
-    char pop()
+    T pop()
     {
         if (top != -1)
         {
@@ -50,11 +50,11 @@ public:
         else
         {
             cout << "Stack underflow!" << endl;
-            return '\0';
+            return '\0';  
         }
     }
 
-    char peek()
+    T peek()
     {
         return (top != -1) ? arr[top] : '\0';
     }
@@ -64,6 +64,7 @@ public:
         return top == -1;
     }
 };
+
 
 int getPrecedence(char op)
 {
@@ -76,40 +77,54 @@ int getPrecedence(char op)
     return 0;
 }
 
+
 bool isOperand(char c)
 {
     return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9');
 }
 
+string manualReverse(string str)
+{
+    string reversed = "";
+    for (int i = str.length() - 1; i >= 0; i--)
+    {
+        reversed += str[i];
+    }
+    return reversed;
+}
+
 string convertToPostfix(string infix)
 {
-    Stack operatorStack(infix.length());
+    Stack<char> operatorStack(infix.length());
     string postfix = "";
 
     for (int i = 0; i < infix.length(); i++)
     {
         char currentChar = infix[i];
 
+    
         if (isOperand(currentChar))
         {
             postfix += currentChar;
         }
+        
         else if (currentChar == '(')
         {
             operatorStack.push(currentChar);
         }
+       
         else if (currentChar == ')')
         {
             while (!operatorStack.isEmpty() && operatorStack.peek() != '(')
             {
                 postfix += operatorStack.pop();
             }
-            operatorStack.pop();
+            operatorStack.pop();  
         }
+        
         else
         {
-            while (!operatorStack.isEmpty() &&
-                   getPrecedence(currentChar) <= getPrecedence(operatorStack.peek()))
+            while (!operatorStack.isEmpty() && getPrecedence(currentChar) <= getPrecedence(operatorStack.peek()))
             {
                 postfix += operatorStack.pop();
             }
@@ -117,6 +132,7 @@ string convertToPostfix(string infix)
         }
     }
 
+    
     while (!operatorStack.isEmpty())
     {
         postfix += operatorStack.pop();
@@ -124,27 +140,40 @@ string convertToPostfix(string infix)
 
     return postfix;
 }
+
+string manualReversePostfix(string postfix)
+{
+    string reversed = "";
+    for (int i = postfix.length() - 1; i >= 0; i--)
+    {
+        reversed += postfix[i];
+    }
+    return reversed;
+}
+
 string convertToPrefix(string infix)
 {
-	reverse(infix.begin(), infix.end());
-	
-	for(int i = 0; i<infix.length(); i++)
-	{
-		if(infix[i]=='(')
-		{
-			infix[i] = ')';
-		}
-		else if(infix[i]=='(')
-		{
-			infix[i] = ')';
-		}
-	}
-	
-	string postFix = convertToPostfix(infix);
-	reverse(postFix.begin(), postFix.end());
-	
-	return postFix;
+    string reversedInfix = manualReverse(infix);
+
+    for (int i = 0; i < reversedInfix.length(); i++)
+    {
+        if (reversedInfix[i] == '(')
+        {
+            reversedInfix[i] = ')';
+        }
+        else if (reversedInfix[i] == ')')
+        {
+            reversedInfix[i] = '(';
+        }
+    }
+
+    string postFix = convertToPostfix(reversedInfix);
+
+    string prefix = manualReversePostfix(postFix);
+
+    return prefix;
 }
+
 int main()
 {
     string infixExpression;
@@ -153,9 +182,11 @@ int main()
     cin >> infixExpression;
 
     string postfixExpression = convertToPostfix(infixExpression);
-    string prefix = convertToPrefix(infixExpression);
-    cout << "The corresponding postfix expression is: " << postfixExpression << endl;
-    cout << "The corresponding prefix expression is: " << postfixExpression << endl;
+
+    string prefixExpression = convertToPrefix(infixExpression);
+
+    cout << "The postfix expression is: " << postfixExpression << endl;
+    cout << "The prefix expression is: " << prefixExpression << endl;
 
     return 0;
 }
